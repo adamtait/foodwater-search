5c7a0024c5532a52997799d184887cf3cd33f206
Adam Tait <adam@adamtait.com>
2014-01-12 23:00:27 -0800

    refactor load-data-from-tumblr ns. change callback route to GET + POST

--- src/com/adamtait/foodwater_search/handler.clj
+++ src/com/adamtait/foodwater_search/handler.clj
@@ -4,15 +4,10 @@
             [compojure.route :as route]
             [com.adamtait.foodwater-search.load-data-from-tumblr :as load-data-from-tumblr]))
 
-(defn pppp [request]
-  (println "request /" request "/")
-  {:status 200
-   :body request})
-
 (defroutes app-routes
   (GET "/" [] "search for ingredients & recipies on http://foodwater.org")
   (GET "/start-oauth" [] (load-data-from-tumblr/new-token-request))
-  (POST "/after-oauth" {params :params} (load-data-from-tumblr/oauth-callback params))
+  (ANY "/after-oauth" {params :params} (load-data-from-tumblr/oauth-callback params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
--- src/com/adamtait/foodwater_search/load_data_from_tumblr.clj
+++ src/com/adamtait/foodwater_search/load_data_from_tumblr.clj
@@ -3,44 +3,68 @@
             [oauth.client :as oauth-client]
             [ring.util.response :as response]))
 
+;;; Tumblr Foodwater-Search App settings
 
-(def foodwater-callback-url "http://199.223.234.98/after-oauth")
-(def foodwater-oauth-key "z5OmNZRMxmhxGkkVqkI7G8uDMIiqsrV3W3WZNj7wH1Cu2MYaic")
-(def foodwater-oauth-secret "683cS4CoWRSe7HU7FSAz3pJAJcEDM4LCpkW8sMuO00YXzDzHAB")
+(def oauth-key "z5OmNZRMxmhxGkkVqkI7G8uDMIiqsrV3W3WZNj7wH1Cu2MYaic")
+(def oauth-secret "683cS4CoWRSe7HU7FSAz3pJAJcEDM4LCpkW8sMuO00YXzDzHAB")
+(def callback-url "http://199.223.234.98/after-oauth")
+
+;;; Tumblr OAuth endpoints
+
 (def tumblr-oauth-endpoint {:request-uri "http://www.tumblr.com/oauth/request_token"
                             :access-uri "http://www.tumblr.com/oauth/access_token"
                             :authorize-uri "http://www.tumblr.com/oauth/authorize"})
 
-(def foodwater-oauth-consumer
-  (oauth-client/make-consumer foodwater-oauth-key
-                              foodwater-oauth-secret
+;;; Tumblr API endpoints
+
+(def info-api "http://api.tumblr.com/v2/blog/foodwater.org/info")
+
+
+
+;;; OAuth structures
+
+(def oauth-consumer
+  (oauth-client/make-consumer oauth-key
+                              oauth-secret
                               (:request-uri tumblr-oauth-endpoint)
                               (:access-uri tumblr-oauth-endpoint)
                               (:authorize-uri tumblr-oauth-endpoint)
                               :hmac-sha1))
 
-(def foodwater-request-token
-  (oauth-client/request-token foodwater-oauth-consumer
-                              foodwater-callback-url))
-
-(def foodwater-info-api "http://api.tumblr.com/v2/blog/foodwater.org/info")
+(defn get-request-token
+  (oauth-client/request-token oauth-consumer
+                              callback-url))
 
 
-(defn new-token-request []
-  (let [tumblr-authorize-uri (oauth-client/user-approval-uri foodwater-oauth-consumer
-                                            (:oauth-token foodwater-request-token))]
+;;; OAuth Flow
+
+(defn new-token-request
+  "the first step in the OAuth handshake. gets a request-token, then redirects user to a Tumblr page to authorize access from the app"
+  []
+  (let [oauth-token (:oauth_token (get-request-token))
+        tumblr-authorize-uri (oauth-client/user-approval-uri oauth-consumer
+                                                             oauth-token)]
     (response/redirect tumblr-authorize-uri)))
 
-(defn oauth-callback [params]
-  (println "callback params /" params "/")
-  (let [access-token-response (oauth-client/access-token foodwater-oauth-consumer
-                                                         foodwater-request-token
-                                                         (:verifier params))
-        credentials (oauth-client/credentials foodwater-oauth-consumer
-                                              (:oauth_token access-token-response)
-                                              (:oauth_token_secret access-token-response)
-                                              :GET
-                                              foodwater-info-api)
-        foodwater-blog-info (http-client/get foodwater-info-api :query-params credentials)]
+(defn oauth-callback
+  "after user authorizes the app, Tumblr lands here. "
+  [params]
+  (let [request-token (get-request-token)
+        access-token-response (oauth-client/access-token oauth-consumer
+                                                         request-token
+                                                         (:verifier params))]
     {:status 200
-     :body foodwater-blog-info}))
+     :body (get-blog-info)}))
+
+
+;;; Tumblr API calls
+
+(defn get-blog-info
+  "request info about the blog from the tumblr api"
+  [oauth-token oauth-secret]
+  (let [credentials (oauth-client/credentials oauth-consumer
+                                              oauth-token
+                                              oauth-secret
+                                              :GET
+                                              info-api)]
+    (http-client/get info-api :query-params credentials)))
