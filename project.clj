383dc082b724e4cbb68da354ca511d39b05fd484
Adam Tait <adam@adamtait.com>
2014-01-13 02:29:48 +0000

    add clj-oauth to project

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+		 [clj-oauth "1.4.1"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler com.adamtait.foodwater-search.handler/app}
   :profiles
