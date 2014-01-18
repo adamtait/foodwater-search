20fcf1cf8ab4fcc2addd09733c03df0745efc6ab
Adam Tait <adam@adamtait.com>
2014-01-18 14:52:56 -0800

    refactor routes into a separate namespace

--- src/com/adamtait/foodwater_search/dispatcher.clj
+++ src/com/adamtait/foodwater_search/dispatcher.clj
@@ -1,13 +1,18 @@
-(ns com.adamtait.foodwater-search.handler
-  (:use compojure.core)
-  (:require [compojure.handler :as handler]
+(ns com.adamtait.foodwater-search.dispatcher
+  (:require [compojure.core :refer [defroutes GET POST ANY]]
+            [compojure.handler :as handler]
             [compojure.route :as route]
+            [com.adamtait.foodwater-search.routes :refer [routes]]
+            [com.adamtait.foodwater-search.views.index :as index]
             [com.adamtait.foodwater-search.load-data-from-tumblr :as load-data-from-tumblr]))
 
 (defroutes app-routes
-  (GET "/" [] "search for ingredients & recipies on http://foodwater.org")
-  (GET "/start-oauth" [] (load-data-from-tumblr/new-token-request))
-  (ANY "/after-oauth" {params :params} (load-data-from-tumblr/oauth-callback params))
+  (GET (:index routes)
+       [] (index/render))
+  (GET (:oauth-auth-new routes)
+       [] (load-data-from-tumblr/new-token-request))
+  (ANY (:oauth-token-new routes)
+       {params :params} (load-data-from-tumblr/oauth-callback params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
---
+++ src/com/adamtait/foodwater_search/routes.clj
@@ -0,0 +1,6 @@
+(ns com.adamtait.foodwater-search.routes)
+
+(def routes
+  {:index "/"
+   :oauth-auth-new "/oauth/auth/new"
+   :oauth-token-new "/oauth/token/new"})
